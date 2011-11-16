a9d4c11479a5e07d558702eced5381a858e48b60
Himani Ahuja <himani.ahuja@sv.cmu.edu>
2011-11-15 17:41:41 -0800

    styling adding ganesha!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-  @images = [ "ss_12.jpg"]
+  @images = [ "img281083.jpg"]
 
   #"ss_10.jpg", "ss_11.jpg","ss_12.jpg", "ss_13.jpg",
 	#     "ss_14.jpg", "ss_15.jpg", "ss_16.jpg"]
