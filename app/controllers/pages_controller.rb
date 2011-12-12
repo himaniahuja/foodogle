e1c03247c2a2c77c39925e8299001f28d608c92f
Himani Ahuja <himani.ahuja@sv.cmu.edu>
2011-12-11 21:32:41 -0800

    image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!, :except => [:home]
 
   def home
-  @images = [ "img281083.jpg"]
+  @images = [ "img.jpg"]
 
   #"ss_10.jpg", "ss_11.jpg","ss_12.jpg", "ss_13.jpg",
 	#     "ss_14.jpg", "ss_15.jpg", "ss_16.jpg"]
+
+
 
   end
 
